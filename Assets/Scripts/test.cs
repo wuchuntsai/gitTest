413b1124127cd7dfd5f5a8ab0603d20eb1a20061
wuchuntsai <wchuntsai@gmail.com>
2024-06-04 22:35:28 +0800

    偵錯

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+  
+    void Start()
+    {
+        Debug.Log("test"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
